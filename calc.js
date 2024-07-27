748d8e54fe393e6a7ba01e86072e4551017e1c18
JM <143664295+thislsnotmyname@users.noreply.github.com>
2024-07-27 15:51:52 -0500

    Update styles and add more logic for operators

--- calc.js
+++ calc.js
@@ -1,41 +1,119 @@
-"use strict";
+'use strict';
 // JM, 07/26/2024
 
-let x = 0;
-let y = 0;
-let operator = 0;
-let result = 0;
+const buttons = document.querySelector('#calculator');
+const display = document.querySelector('#display');
+const operators = [
+    '+',
+    '-',
+    'x',
 
-let add = (x, y) => {
+];
+
+let x = '';
+let y = '';
+let operator = '';
+let result = 0;
+let displayText = '';
+
+let add = function(x, y) {
     return x + y;
 }
 
-let subtract = (x, y) => {
+let subtract = function(x, y) {
     return x - y;
 }
 
-let multiply = (x, y) => {
+let multiply = function(x, y) {
     return x * y;
 }
 
-let divide = (x, y) => {
-    if (y === 0) return "Nuh-uh-uh!";
+let divide = function(x, y) {
+    if (y === 0) return 'Nuh-uh-uh!';
     return x / y;
 }
 
-let equals = (x, y, operator) => {
+let equals = function(x, y, operator) {
+    let result;
     switch (operator) {
-        case "plus":
-            return add(x, y);
-        case "minus":
-            return subtract(x, y);
-        case "multiply":
-            return multiply(x, y);
-        case "divide":
-            return divide(x, y);
+        case 'plus':
+            result = add(x, y);
+            break;
+        case 'minus':
+            result = subtract(x, y);
+            break;
+        case 'multiply':
+            result = multiply(x, y);
+            break;
+        case 'divide':
+            result = divide(x, y);
+            break;
         default:
-            return "ERROR: No operator defined.";
+            return 'ERROR: No operator defined.';
     }
+    return result;
 }
 
-console.log(equals(0, 0, "divide"));
+let updateText = function() {
+    if (x === '') return '';
+    if (operator === '') {
+        return `${x}`;
+    } else if (y === '') {
+        return `${x} ${operator}`;
+    }
+    
+    console.table(x, y, operator);
+
+    return displayText = `${x} ${operator} ${y}`;
+}
+
+buttons.addEventListener('click', (e) => {
+    if (e.target.nodeName !== 'BUTTON') return;
+
+    if (e.target.id[0] === '_') {
+        if (operator === '') {
+            x = Number(`${x}${e.target.id[1]}`);
+        } else {
+            y = Number(`${y}${e.target.id[1]}`);
+        }
+    } else if (x !== '') {
+        switch (e.target.id) {
+            case "dec":
+
+                break;
+            case "clear":
+                x = '';
+                y = '';
+                operator = '';
+                break;
+            case "back":
+                if (operator === '') {
+                    x = x.toString();
+                    x = Number(x.slice(0, x.length - 1));
+                } else {
+                    y = y.toString();
+                    y = Number(y.slice(0, y.length - 1));
+                }
+                break;
+            case 'equals':
+                if (y === '') {
+                    break;
+                } else {
+                    x = equals(x, y, operator);
+                    y = '';
+                    operator = '';
+                }
+            break;
+            default:
+                if (operator === '') {
+                    operator = e.target.id;
+                } else if (y !== '') {
+                    x = equals(x, y, operator);
+                    y = '';
+                    operator = e.target.id;
+                }
+        }
+    }
+
+    display.textContent = updateText();
+});
